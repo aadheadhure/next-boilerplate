7fb44c43fd0ea1078bdcbb6fa9b32d2a0922537c
Arnav Gosain <arnav@arnavgosain.com>
2020-12-30 03:53:54 +0530

    Split css files for faster tailwind compilation
    Signed-off-by: Arnav Gosain <arnav@arnavgosain.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from "next/app";
+import "~/styles/tailwind.css";
 import "../styles/index.css";
+import "~/styles/tailwind-utils.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
